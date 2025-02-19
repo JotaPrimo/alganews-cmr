ece4184ae2f93bbb5a33e5b160d671cbde12f61c
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-19 13:05:24 -0400

    4.6. Criando o boilerplate do Botão

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import * as B from './Button.styles'
+
+export interface ButtonProps {
+
+}
+
+function Button({}: ButtonProps) {
+    return <B.Wrapper>
+        botão
+    </B.Wrapper>;
+}
+
+export default Button;
